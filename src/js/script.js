8d27c6e0bb74763a93ab281efad8acb2b0025b08
SHVM-09 <monkeydeluffy98@icloud.com>
2024-01-02 19:36:48 +0530

    feat(init): added landing page & assets

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+const formCloseBtn = document.querySelectorAll('.form-close');
+const formModal = document.getElementById('form-modal');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('hidden');
+    nav.classList.toggle('flex');
+    btn.classList.toggle('open');
+    });
+
+formCloseBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+       formModal.classList.toggle('hidden');
+    });
+}   ); 
+
+formModal.onsubmit = (e) => {    
+    formModal.classList.toggle('hidden');
+}
